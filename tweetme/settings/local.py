96fc14ba6b111ac1dc568b6d3d332ab2f660db0d
brainchen2020 <lukcyadamchen@gmail.com>
2018-11-06 17:31:11 +0800

    Add files via upload

--- tweetme/settings/local.py
+++ tweetme/settings/local.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '$ak3=6=*(@8)x!k_v_2eh=m54o@g=!u-ef1=ks-_m+a5e$acvd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
